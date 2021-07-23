4170090d15ce433439e7ebaae5baed061976d55d
Cristian Gutierrez <cdgutierrez456@gmail.com>
2021-07-23 11:31:20 -0500

    Mostrando el resultado en pantalla con informacion importante

--- src/components/Resultado.js
+++ src/components/Resultado.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
-const Resultado = () => {
+const Resultado = ({total, cantidad, plazo}) => {
     return (
-        <p>Desde resultado</p>
+        <div className="u-full-width resultado">
+            <h2>Resumen</h2>
+            <p>La cantidad solicitada es: $ {cantidad}</p>
+            <p>A pagar en: {plazo} Meses</p>
+            <p>Su pago mensual es de: ${Math.ceil(total / plazo)}</p>
+            <p>Total a pagar: $ {(total).toFixed(2)}</p>
+        </div>
     );
 }
 
